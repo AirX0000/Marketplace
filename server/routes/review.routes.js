17651b8db4888b957b62d201001c70c6ff2b04cc
ㅤAir ㅤ <air@MacBook-Air-Air.local>
2026-02-26 14:58:14 +0500

    fix: restore auth and review routes

---
+++ server/routes/review.routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const reviewController = require('../controllers/review.controller');
+const { authenticateToken } = require('../middleware/auth.middleware');
+
+router.get('/marketplace/:id', reviewController.getMarketplaceReviews);
+router.post('/marketplace/:id', authenticateToken, reviewController.createReview);
+router.delete('/:reviewId', authenticateToken, reviewController.deleteReview);
+
+module.exports = router;
